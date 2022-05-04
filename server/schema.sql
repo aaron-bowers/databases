0d6972c148d525fca71978e63ff3a0abc68bcc2e
Aaron Bowers <bowersaaronjames@gmail.com>
2022-05-04 16:33:07 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,34 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  message_text VARCHAR(1000),
+  message_timestamp TIMESTAMP,
+  id_users INT,
+  id_rooms INT,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  name INT(30),
+  PRIMARY KEY (id)
+);
 
-
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id_users INT,
+  id_rooms INT
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
